4de8052aab0061fad6c0044f46dc41e583f5218f
Max Millington <mmillington@paypal.com>
2017-12-08 10:14:21 -0800

    interview boilerplate

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+
+// 'https://swapi.co/api/people'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="outer-container">
+        <div className="header">
+          Hey let's talk star wars
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
